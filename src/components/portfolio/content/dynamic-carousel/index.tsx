1f6be85218a1526cffe496fafc115f8ef293394a
manjushsh <manjushsh@protonmail.com>
2022-03-18 09:04:17 +0530

    UI Changes

--- src/components/portfolio/content/dynamic-carousel/index.tsx
+++ src/components/portfolio/content/dynamic-carousel/index.tsx
@@ -6,7 +6,7 @@ const GalleryView = () => {
 
     const [imageURL, updateImageURL] = useState(colorfulImage);
     const fetchRandomImage = () => {
-        const URL = `${process.env.REACT_APP_UNSPLASH_API_URL}/photos/random?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`;
+        const URL = `${process.env.REACT_APP_UNSPLASH_API_URL}/photos/random?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}&&orientation=landscape`;
         fetch(URL)
             .then(response => response.json())
             .then(imageDetails => updateImageURL(imageDetails?.urls?.regular))
@@ -16,14 +16,11 @@ const GalleryView = () => {
     };
 
     return <>
-        <div className='background' style={{
-            background: `url(${imageURL}) no-repeat`,
-            backgroundSize: 'cover',
-        }}></div>
-        <div className='foreground' style={{height: '100vh'}}>
+        <div className='background'></div>
+        <div className='foreground' style={{ height: '100vh' }}>
             <div className='galleryContent flex-vh-centered'>
                 <div className='image-card'>
-                    <div className="card" style={{ width: '20rem' }}>
+                    <div className="card" style={{ width: '100vh' }}>
                         <img className="card-img-top" src={imageURL} alt='Wallpaper' />
                     </div>
                 </div>
